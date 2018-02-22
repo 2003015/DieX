ee98e7bc0a60b3c408fd69a6065054116efd20cd
2007015 <2007015@geneva304.org>
2018-02-22 14:46:18 -0600

    ALPHA 1.9

--- app/src/main/java/club/theexperiment/diex/d2.java
+++ app/src/main/java/club/theexperiment/diex/d2.java
@@ -1,10 +1,11 @@
 package club.theexperiment.diex;
 
 public class d2 extends Die {
-
-
     public d2(int r) {
-        int sides = 2;
+        setSides(2);
+        setNumberOfDice(r);
+
+
 
     }
 
