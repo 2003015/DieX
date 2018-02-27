cd374d9c1864b2c5374d30a3ea993bcadd4cdc1e
2003015 <2003015@geneva304.org>
2018-02-27 14:30:22 -0600

    Progress on defaultFragment

--- app/src/main/java/club/theexperiment/diex/defaultFragment.java
+++ app/src/main/java/club/theexperiment/diex/defaultFragment.java
@@ -6,23 +6,32 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
-public class defaultFragment {
-    Button mD2Button = (Button) findViewById(R.id.d2Button);
-    mD2Button.setOnClickListener(new View.OnClickListener() {
+public class defaultFragment extends Fragment {
 
-        @Override
-        public void onClick(View v) {
-            Log.w("d2","Fire1");
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            Log.w("d2","Fire2");
-            ft.replace(R.id.content_frame, new d2Fragment());
-            Log.w("d2","Fire3");
-            ft.commit();
-            Log.w("d2","Fire4");
-        }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View rootView = inflater.inflate(R.layout.fragment_default, container, false);
+        Button mD2Button = (Button) rootView.findViewById(R.id.d2Button);
+        mD2Button.setOnClickListener(new View.OnClickListener() {
 
-    });
+            @Override
+            public void onClick(View v) {
+                Log.w("d2","Fire1");
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                Log.w("d2","Fire2");
+                ft.replace(R.id.content_frame, new d2Fragment());
+                Log.w("d2","Fire3");
+                ft.commit();
+                Log.w("d2","Fire4");
+            }
+
+        });
+        return rootView;
+    }
 }
