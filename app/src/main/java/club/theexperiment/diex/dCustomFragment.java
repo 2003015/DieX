17ed51d7042ba64794d932e4cb288afbe942e868
2003015 <2003015@geneva304.org>
2018-03-14 14:34:17 -0500

    Fied dcustom layout resource name

--- app/src/main/java/club/theexperiment/diex/dCustomFragment.java
+++ app/src/main/java/club/theexperiment/diex/dCustomFragment.java
@@ -13,7 +13,7 @@ public class dCustomFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        View rootView = inflater.inflate(R.layout.fragment_dCustom, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_dcustom, container, false);
         return rootView;
     }
 
