16b1afe899d23c93a3ccb51c3f1035022ab5bb54
2007028 <2007028@geneva304.org>
2018-03-16 14:46:50 -0500

    Creating ListView

--- app/src/main/java/club/theexperiment/diex/d2Fragment.java
+++ app/src/main/java/club/theexperiment/diex/d2Fragment.java
@@ -5,8 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 public class d2Fragment extends Fragment {
 
@@ -20,12 +22,10 @@ public class d2Fragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         final View rootView = inflater.inflate(R.layout.fragment_d2, container, false);
 
-
         Dice = new d2();
-
+        final ListView listView = (ListView) rootView.findViewById(R.id.d2ListView);
         Button mD2RollButton = (Button) rootView.findViewById(R.id.d2RollButton);
         mD2RollButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 numDice = ((EditText) rootView.findViewById(R.id.d2Rolls)).getText().toString();
@@ -33,6 +33,16 @@ public class d2Fragment extends Fragment {
                 nD = Integer.parseInt(numDice);
                 Dice.setNumberOfDice(nD);
                 Dice.roll(nD);
+                String[] rollStrings = new String[Dice.getRolls().length];
+                for (int i = 0; i <= rollStrings.length; i++) {
+
+                    rollStrings[i] = Integer.toString(Dice.getRolls()[i]);
+
+                }
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter <String> (this, android.R.layout.simple_list_item_1, rollStrings);
+
+                listView.setAdapter(arrayAdapter);
+
             }
 
         });
