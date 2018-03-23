e256bc4f3bc19f576531869c6f69eb6aecbfeb9e
2003015 <2003015@geneva304.org>
2018-03-23 14:45:27 -0500

    Need desperately to fix listview displaying items, currently all blank

--- app/src/main/java/club/theexperiment/diex/d2Fragment.java
+++ app/src/main/java/club/theexperiment/diex/d2Fragment.java
@@ -2,6 +2,7 @@ package club.theexperiment.diex;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,12 +11,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class d2Fragment extends Fragment {
 
     private d2 Dice;
     private String numDice;
     private int nD;
     private static final String TAG = "ProfileFragment";
+    private List<String> rollStrings = new ArrayList<>();
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +31,7 @@ public class d2Fragment extends Fragment {
         Dice = new d2();
         final ListView listView = (ListView) rootView.findViewById(R.id.d2ListView);
         Button mD2RollButton = (Button) rootView.findViewById(R.id.d2RollButton);
+        //final List<String> rollStrings = new ArrayList<>();
         mD2RollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -33,14 +40,15 @@ public class d2Fragment extends Fragment {
                 nD = Integer.parseInt(numDice);
                 Dice.setNumberOfDice(nD);
                 Dice.roll(nD);
-                String[] rollStrings = new String[Dice.getRolls().length];
-                for (int i = 0; i <= rollStrings.length; i++) {
+                //String[] rollStrings = new String[Dice.getRolls().length];
+                for (int i = 0; i < Dice.getRolls().length; i++) {
 
-                    rollStrings[i] = Integer.toString(Dice.getRolls()[i]);
+                    //rollStrings[i] = Integer.toString(Dice.getRolls()[i]);
+                    rollStrings.add(Integer.toString(Dice.getRolls()[i]));
+                    Log.e("test", Integer.toString(Dice.getRolls()[i]));
 
                 }
-                ArrayAdapter<String> arrayAdapter = new ArrayAdapter <String> (this, android.R.layout.simple_list_item_1, rollStrings);
-
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<> (getActivity().getApplicationContext(), R.layout.listview, rollStrings);
                 listView.setAdapter(arrayAdapter);
 
             }
