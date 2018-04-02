4b0c6901d09a0d5d9746fdc203549ec20fcb0520
Reese <2003015@geneva304.org>
2018-04-01 19:43:12 -0500

    Shows number of times each time rolled for d2

--- app/src/main/java/club/theexperiment/diex/d2Fragment.java
+++ app/src/main/java/club/theexperiment/diex/d2Fragment.java
@@ -1,7 +1,9 @@
 package club.theexperiment.diex;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,24 +16,19 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class d2Fragment extends Fragment {
+public class d2Fragment extends ListFragment {
 
     private d2 Dice;
     private String numDice;
     private int nD;
-    private static final String TAG = "ProfileFragment";
-    private List<String> rollStrings = new ArrayList<>();
-
+    private Button mD2RollButton;
+    private String[] rollStrings;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
         final View rootView = inflater.inflate(R.layout.fragment_d2, container, false);
-
+        mD2RollButton = (Button) rootView.findViewById(R.id.d2RollButton);
         Dice = new d2();
-        final ListView listView = (ListView) rootView.findViewById(R.id.d2ListView);
-        Button mD2RollButton = (Button) rootView.findViewById(R.id.d2RollButton);
-        //final List<String> rollStrings = new ArrayList<>();
         mD2RollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,28 +37,21 @@ public class d2Fragment extends Fragment {
                 nD = Integer.parseInt(numDice);
                 Dice.setNumberOfDice(nD);
                 Dice.roll(nD);
+                rollStrings = new String[Dice.getRolls().length];
                 //String[] rollStrings = new String[Dice.getRolls().length];
                 for (int i = 0; i < Dice.getRolls().length; i++) {
 
                     //rollStrings[i] = Integer.toString(Dice.getRolls()[i]);
-                    rollStrings.add(Integer.toString(Dice.getRolls()[i]));
+                    rollStrings[i] = Integer.toString(Dice.getRolls()[i]);
                     Log.e("test", Integer.toString(Dice.getRolls()[i]));
 
                 }
-                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<> (getActivity().getApplicationContext(), R.layout.listview, rollStrings);
-                listView.setAdapter(arrayAdapter);
-
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String> (getActivity(), R.layout.listview, rollStrings);
+                setListAdapter(arrayAdapter);
             }
 
         });
-
-        /// use .getRolls
-
-
-
-
         return rootView;
+
     }
-
-
 }
