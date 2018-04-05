50c285e0e4e9c4380d7d36174372a22755e2dfa4
2003015 <2003015@geneva304.org>
2018-04-05 14:43:12 -0500

    Comments

--- app/src/main/java/club/theexperiment/diex/MainActivity.java
+++ app/src/main/java/club/theexperiment/diex/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        actionBar.setDisplayHomeAsUpEnabled(true);
         //Load fragment_default onto screen
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
