2a8367b9354590782d63c1084ddb9301ca8f6774
2007015 <2007015@geneva304.org>
2018-04-06 14:41:53 -0500

    Starting video work

--- app/src/main/java/club/theexperiment/diex/MainActivity.java
+++ app/src/main/java/club/theexperiment/diex/MainActivity.java
@@ -7,6 +7,11 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
 
 public class MainActivity extends AppCompatActivity {
     //Create Die instance to use for rolls
@@ -17,13 +22,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Create back button
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        VideoView videoView;
+        Uri uri;
 
         //Load fragment_default onto screen
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.content_frame, new defaultFragment());
         fragmentTransaction.commit();
+
+           videoView = (VideoView) findViewById(R.id.video_view);
+
+            uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.d1_dice);
+            videoView.setVideoURI(uri);
+
     }
 }
