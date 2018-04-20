b79c9f535bcd9959ef009a143a468f3925d81556
1806357 <1806357@geneva304.org>
2018-04-20 14:46:11 -0500

    poysh

--- app/src/main/java/club/theexperiment/diex/BotBoi.java
+++ app/src/main/java/club/theexperiment/diex/BotBoi.java
@@ -1,14 +1,20 @@
 package club.theexperiment.diex;
 
+import java.util.List;
+
 /**
  * Created by 1806357 on 4/20/2018.
  */
 
 public class BotBoi {
 
-
+    private int[][] store;
+    private List nums;
 
     public BotBoi(){
-
+        store = new int[][]{{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0},{0,0,0}};
     }
+
+    public void sto
+
 }
