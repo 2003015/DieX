b0788fbde52c13aab115fc27c52b588422195bdc
1806357 <1806357@geneva304.org>
2018-04-23 14:04:06 -0500

    poysh

--- app/src/main/java/club/theexperiment/diex/BotBoi.java
+++ app/src/main/java/club/theexperiment/diex/BotBoi.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class BotBoi {
 
-    private int[][] store;
+    private int[][] store; //option, sides, usual amount rolled
     private List nums;
 
     public BotBoi(){
